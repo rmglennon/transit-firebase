6c221c3432151a86c4ba57efac8dba7eda8437e0
Rhonda Glennon <rhondaglennon@gmail.com>
2018-02-05 21:23:31 -0800

    improve formatting, clean up code

--- assets/javascript/transit.js
+++ assets/javascript/transit.js
@@ -38,12 +38,6 @@ $("#train-form").on("submit", function() {
   frequency = $("#frequency").val().trim();
   firstTrain = $("#first-train").val().trim();
 
-  // test everything
-  console.log(name);
-  console.log(destination);
-  console.log(frequency);
-  console.log(firstTrain);
-
   // create nodes in Firebase with variable names
   database.ref().push({
     name: name,
@@ -60,87 +54,27 @@ $("#train-form").on("submit", function() {
 // run when a new child is added
 database.ref().on("child_added", function(snapshot) {
 
-  // log all the snapshot values
-  console.log("snapshot name: " + snapshot.val().name);
-  console.log("snapshot destination: " + snapshot.val().destination);
-  console.log("snapshot frequency: " + snapshot.val().frequency);
-  console.log("snapshot first train: " + snapshot.val().firstTrain);
-
-  // // use moment.js to calculate the train times by calculating the entire day's schedule
-  //
-  // // make variables pointing to database snapshot values for simplicity
+  // make new variables pointing to database snapshot values for simplicity
   var freq = snapshot.val().frequency;
   var initialTime = snapshot.val().firstTrain;
 
+  // call functions to calculate the next train and arrival times
   var nextTrain = calculateNextTrains(freq, initialTime);
+  var times = calculateArrivalTime(nextTrain);
+  var formattedAMPM = times[0];
+  var minutesAway = times[1];
 
+  // update the list of upcoming trains
 
-var times = calculateArrivalTime(nextTrain);
-var formattedAMPM = times[0];
-var minutesAway = times[1];
-
-
-
-  //
-  // // create a moment for initial time and format it
-  // var timeMoment = moment(initialTime, "HH:mm");
-  //
-  // // create a moment for the time at the end of the day
-  // var endOfDay = moment("23:59", "HH:mm");
-  //
-  // // creare an empty array to hold all train departure times for the day
-  // var timetable = [];
-  //
-  // // create a timetable array for the day by adding frequency to first train time
-  // // Note: this assumes the train runs at the same frequency all day
-  // for (var i = timeMoment; i.isSameOrBefore(endOfDay); i.add(freq, "minutes")) {
-  //   var times = i.format("HH:mm");
-  //   timetable.push(times);
-  // }
-  //
-  // console.log(timetable);
-  //
-  // // create a variable representing now as a new moment
-  // var now = moment();
-  //
-  // // create a variable to hold trains that depart after the current time
-  // var futureTrains = [];
-  //
-  // // only look for trains in the future and find next one
-  // for (var i = 0; i < timetable.length; i++) {
-  //
-  //   if (moment(timetable[i], "HH:mm").isAfter(now)) {
-  //     futureTrains.push(timetable[i]);
-  //   }
-  // }
-  //
-  // console.log(futureTrains);
-  // the next train is the first one [0] in the array of departures remaining for the day
-  // var nextTrain = futureTrains[0];
-  // console.log(nextTrain);
-
-  // use moment.js to calculate the difference between now and the next train and at what time it will depart (formatted in AM/PM style)
-  // var now = moment();
-  // var nextTrain = calculateNextTrains(freq, initialTime);
-  // var minutesAway = moment(nextTrain, "HH:mm").diff(now, "minutes");
-  // console.log(minutesAway);
-  //
-  // var formattedAMPM = moment(nextTrain, "HH:mm").format("h:mm a");
-  // console.log(formattedAMPM);
-
-
-
-  // update the user interface list of upcoming trains
-
-  // create a new table row
+  // create a new table row and table data in it
   var newTableRow = $("<tr>");
 
   var newTableData =
-  $("<td>" + snapshot.val().name + "</td>" +
-  "<td>" + snapshot.val().destination + "</td>" +
-  "<td>" + snapshot.val().frequency + "</td>" +
-  "<td>" + formattedAMPM + "</td>"+
-  "<td>" + minutesAway + "</td>");
+  $("<td id='table-train-name'>" + snapshot.val().name + "</td>" +
+  "<td id='table-train-destination'>" + snapshot.val().destination + "</td>" +
+  "<td id='table-train-frequency'>" + snapshot.val().frequency + "</td>" +
+  "<td id='table-train-arrival'>" + formattedAMPM + "</td>"+
+  "<td id='table-train-minutes'>" + minutesAway + "</td>");
 
   // append the row and data to the table body to display on the page
   newTableRow.append(newTableData);
@@ -148,10 +82,8 @@ var minutesAway = times[1];
 
 });
 
-
-
+// use moment.js to calculate the train times by building the entire day's schedule
 function calculateNextTrains(freq, initialTime) {
-  // use moment.js to calculate the train times by calculating the entire day's schedule
 
   // create a moment for initial time and format it
   var timeMoment = moment(initialTime, "HH:mm");
@@ -159,7 +91,7 @@ function calculateNextTrains(freq, initialTime) {
   // create a moment for the time at the end of the day
   var endOfDay = moment("23:59", "HH:mm");
 
-  // creare an empty array to hold all train departure times for the day
+  // create an empty array to hold all train departure times for the day
   var timetable = [];
 
   // create a timetable array for the day by adding frequency to first train time
@@ -169,8 +101,6 @@ function calculateNextTrains(freq, initialTime) {
     timetable.push(times);
   }
 
-  console.log(timetable);
-
   // create a variable representing now as a new moment
   var now = moment();
 
@@ -179,33 +109,25 @@ function calculateNextTrains(freq, initialTime) {
 
   // only look for trains in the future and find next one
   for (var i = 0; i < timetable.length; i++) {
-
     if (moment(timetable[i], "HH:mm").isAfter(now)) {
       futureTrains.push(timetable[i]);
     }
   }
 
-  console.log(futureTrains);
   // the next train is the first one [0] in the array of departures remaining for the day
   var nextTrain = futureTrains[0];
-  console.log(nextTrain);
 
-return nextTrain;
+  return nextTrain;
 
 }
 
-
+// use moment.js to calculate the minutes until the next train
 function calculateArrivalTime(nextTrain) {
   var now = moment();
   var minutesAway = moment(nextTrain, "HH:mm").diff(now, "minutes");
   var formattedAMPM = moment(nextTrain, "HH:mm").format("h:mm a");
-  console.log("from calculateArrivalTime " + formattedAMPM);
-  console.log("from calculateArrivalTime " + minutesAway);
   return [formattedAMPM, minutesAway];
 }
-
-
-
 
 // validate the items on the train input form
 // "require" is set on the html fields to ensure there is an input
@@ -216,12 +138,10 @@ function validateForm() {
   if (!validateFirstTrainTime(firstTrainTimes[0], firstTrainTimes[1])) {
     return false;
   }
-
   return true;
 }
 
 // validate that the hours and minutes are within expected range
-
 function validateFirstTrainTime(hours, minutes) {
   if (!((hours >= 00 || hours >= 0) && (hours <= 23))) {
     return false;
